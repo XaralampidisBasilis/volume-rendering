369d9078f1c3d4dd0e573ccd895b0ae31ccea3f6
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2024-10-07 18:27:04 +0300

    in depth pade exploration

--- src/shaders/viewers/iso_viewer/chunks/stepping/scripts/pade22_approximation.m
+++ src/shaders/viewers/iso_viewer/chunks/stepping/scripts/pade22_approximation.m
@@ -1,20 +1,18 @@
 clear, clc
-syms a0 a1 a2 b1 x x1 f01 f11 f21 f31 f41 fc
-assume([ a0 a1 a2 b1 x x1 f01 f11 f21 f31 f41 fc], 'real')
+syms a0 b1 b2 b3 x x0 f00 f10 f20 f30 f40 fc
+assume([a0 b1 b2 b3 x x0 f00 f10 f20 f30 f40 fc], 'real')
 
-padde = (a0 + a1 * x + a2 * x^2) / (1 + b1 * x);
-
-taylor = f01 + f11 / factorial(1) * (x - x1) + f21 / factorial(2) * (x - x1)^2 + f31 / factorial(3) * (x - x1)^3 + f41 / factorial(4) * (x - x1)^4;
-pade22 = pade(taylor, x, 'Order', [2 2], 'ExpansionPoint', x1);
+taylor = f00 + f10 / factorial(1) * (x - x0) + f20 / factorial(2) * (x - x0)^2 + f30 / factorial(3) * (x - x0)^3 + f40 / factorial(4) * (x - x0)^4;
+pade22 = pade(taylor, x, 'Order', [2 2], 'ExpansionPoint', x0);
 
 %% Simplify pade21
-pade22 = simplify(subs(pade22, [x1, f41], [0, 0]));
+pade22 = simplify(subs(pade22, [x0, f40], [0, 0]));
 [num, den] = numden(pade22);
 [cubic_coeffs, x_terms] = coeffs(num - den * fc, x);
  cubic_coeffs = simplify(cubic_coeffs);
 
 syms err
-cubic_coeffs = subs(cubic_coeffs, fc, f01 - err);
+cubic_coeffs = subs(cubic_coeffs, fc, f00 - err);
 cubic_coeffs = arrayfun(@(x) simplify(x), cubic_coeffs);
 
 pretty(cubic_coeffs')
